bb9efbf9f88c3c8f426cf3095b3a3811a8ca99a7
Chris Gilmer <chris@truss.works>
2019-06-19 09:03:02 -0700

    Update module to terraform 0.12 syntax

--- main.tf
+++ main.tf
@@ -38,18 +38,18 @@
 resource "aws_security_group" "alb_sg" {
   name        = "alb-${var.name}-${var.environment}"
   description = "${var.name}-${var.environment} ALB security group"
-  vpc_id      = "${var.alb_vpc_id}"
+  vpc_id      = var.alb_vpc_id
 
   tags = {
     Name        = "alb-${var.name}-${var.environment}"
-    Environment = "${var.environment}"
+    Environment = var.environment
     Automation  = "Terraform"
   }
 }
 
 resource "aws_security_group_rule" "app_alb_allow_outbound" {
   description       = "All outbound"
-  security_group_id = "${aws_security_group.alb_sg.id}"
+  security_group_id = aws_security_group.alb_sg.id
 
   type        = "egress"
   from_port   = 0
@@ -59,10 +59,10 @@ resource "aws_security_group_rule" "app_alb_allow_outbound" {
 }
 
 resource "aws_security_group_rule" "app_alb_allow_https_from_world" {
-  count = "${var.allow_public_https ? 1 : 0}"
+  count = var.allow_public_https ? 1 : 0
 
   description       = "Allow in HTTPS"
-  security_group_id = "${aws_security_group.alb_sg.id}"
+  security_group_id = aws_security_group.alb_sg.id
 
   type        = "ingress"
   from_port   = 443
@@ -72,10 +72,10 @@ resource "aws_security_group_rule" "app_alb_allow_https_from_world" {
 }
 
 resource "aws_security_group_rule" "app_alb_allow_http_from_world" {
-  count = "${var.allow_public_http ? 1 : 0}"
+  count = var.allow_public_http ? 1 : 0
 
   description       = "Allow in HTTP"
-  security_group_id = "${aws_security_group.alb_sg.id}"
+  security_group_id = aws_security_group.alb_sg.id
 
   type        = "ingress"
   from_port   = 80
@@ -90,54 +90,54 @@ resource "aws_security_group_rule" "app_alb_allow_http_from_world" {
 
 resource "aws_lb" "main" {
   name            = "${var.name}-${var.environment}"
-  internal        = "${var.alb_internal}"
-  subnets         = "${var.alb_subnet_ids}"
-  security_groups = ["${aws_security_group.alb_sg.id}"]
+  internal        = var.alb_internal
+  subnets         = var.alb_subnet_ids
+  security_groups = [aws_security_group.alb_sg.id]
 
   access_logs {
     enabled = true
-    bucket  = "${var.logs_s3_bucket}"
+    bucket  = var.logs_s3_bucket
     prefix  = "alb/${var.name}-${var.environment}"
   }
 
   tags = {
-    Environment = "${var.environment}"
+    Environment = var.environment
     Automation  = "Terraform"
   }
 }
 
 resource "aws_lb_target_group" "https" {
   name        = "ecs-${var.name}-${var.environment}-https"
-  port        = "${var.container_port}"
-  protocol    = "${var.container_protocol}"
-  vpc_id      = "${var.alb_vpc_id}"
+  port        = var.container_port
+  protocol    = var.container_protocol
+  vpc_id      = var.alb_vpc_id
   target_type = "ip"
 
   # The amount time for the LB to wait before changing the state of a
   # deregistering target from draining to unused. AWS default is 300 seconds.
-  deregistration_delay = "${var.deregistration_delay}"
+  deregistration_delay = var.deregistration_delay
 
   health_check {
-    timeout             = "${var.health_check_timeout}"
-    interval            = "${var.health_check_interval}"
-    path                = "${var.health_check_path}"
-    protocol            = "${var.container_protocol}"
-    healthy_threshold   = "${var.healthy_threshold}"
-    unhealthy_threshold = "${var.unhealthy_threshold}"
-    matcher             = "${var.health_check_success_codes}"
+    timeout             = var.health_check_timeout
+    interval            = var.health_check_interval
+    path                = var.health_check_path
+    protocol            = var.container_protocol
+    healthy_threshold   = var.healthy_threshold
+    unhealthy_threshold = var.unhealthy_threshold
+    matcher             = var.health_check_success_codes
   }
 
   # Ensure the ALB exists before things start referencing this target group.
-  depends_on = ["aws_lb.main"]
+  depends_on = [aws_lb.main]
 
   tags = {
-    Environment = "${var.environment}"
+    Environment = var.environment
     Automation  = "Terraform"
   }
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = "${aws_lb.main.id}"
+  load_balancer_arn = aws_lb.main.id
   port              = "80"
   protocol          = "HTTP"
 
@@ -153,20 +153,21 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_listener" "https" {
-  load_balancer_arn = "${aws_lb.main.id}"
+  load_balancer_arn = aws_lb.main.id
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "${var.alb_ssl_policy}"
-  certificate_arn   = "${var.alb_default_certificate_arn}"
+  ssl_policy        = var.alb_ssl_policy
+  certificate_arn   = var.alb_default_certificate_arn
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.https.id}"
+    target_group_arn = aws_lb_target_group.https.id
     type             = "forward"
   }
 }
 
 resource "aws_lb_listener_certificate" "main" {
-  count           = "${length(var.alb_certificate_arns)}"
-  listener_arn    = "${aws_lb_listener.https.arn}"
-  certificate_arn = "${element(var.alb_certificate_arns, count.index)}"
+  count           = length(var.alb_certificate_arns)
+  listener_arn    = aws_lb_listener.https.arn
+  certificate_arn = element(var.alb_certificate_arns, count.index)
 }
+
--- variables.tf
+++ variables.tf
@@ -1,113 +1,114 @@
 variable "name" {
   description = "The service name."
-  type        = "string"
+  type        = string
 }
 
 variable "environment" {
   description = "Environment tag, e.g prod."
-  type        = "string"
+  type        = string
 }
 
 variable "logs_s3_bucket" {
   description = "S3 bucket for storing Application Load Balancer logs."
-  type        = "string"
+  type        = string
 }
 
 variable "alb_default_certificate_arn" {
   description = "The ARN of the default certificate to be attached to the ALB."
-  type        = "string"
+  type        = string
 }
 
 variable "alb_certificate_arns" {
   description = "The ARNs of the certificates to be attached to the ALB."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "alb_vpc_id" {
   description = "VPC ID to be used by the ALB."
-  type        = "string"
+  type        = string
 }
 
 variable "alb_internal" {
   description = "If true, the ALB will be internal. Default's to false, the ALB will be public."
-  type        = "string"
+  type        = string
   default     = false
 }
 
 variable "alb_subnet_ids" {
   description = "Subnet IDs for the ALB. Use public subnets for a public ALB and private subnets for an internal ALB."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "alb_ssl_policy" {
   description = "The SSL policy (aka security policy) for the Application Load Balancer that specifies the TLS protocols and ciphers allowed.  See <https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html#describe-ssl-policies>."
-  type        = "string"
+  type        = string
   default     = "ELBSecurityPolicy-2016-08"
 }
 
 variable "deregistration_delay" {
   description = "The amount time for the LB to wait before changing the state of a deregistering target from draining to unused. Default is 90s."
-  type        = "string"
+  type        = string
   default     = 90
 }
 
 variable "health_check_interval" {
   description = "The approximate amount of time, in seconds, between health checks of an individual target. Minimum value 5 seconds, Maximum value 300 seconds. Default 30 seconds."
-  type        = "string"
+  type        = string
   default     = 30
 }
 
 variable "health_check_timeout" {
   description = "The health check timeout. Minimum value 2 seconds, Maximum value 60 seconds. Default 5 seconds."
-  type        = "string"
+  type        = string
   default     = 5
 }
 
 variable "health_check_path" {
   description = "The destination for the health check requests to the container."
-  type        = "string"
+  type        = string
   default     = "/"
 }
 
 variable "healthy_threshold" {
   description = "The number of consecutive health checks successes required before considering an unhealthy target healthy. Defaults to 3."
-  type        = "string"
+  type        = string
   default     = 3
 }
 
 variable "unhealthy_threshold" {
   description = "The number of consecutive health check failures required before considering the target unhealthy. For Network Load Balancers, this value must be the same as the healthy_threshold. Defaults to 3."
-  type        = "string"
+  type        = string
   default     = 3
 }
 
 variable "health_check_success_codes" {
   description = "The HTTP codes to use when checking for a successful response from the container. You can specify multiple values (for example, '200,202') or a range of values (for example, '200-299')."
-  type        = "string"
+  type        = string
   default     = "200"
 }
 
 variable "container_port" {
   description = "The port on which the container will receive traffic."
-  type        = "string"
+  type        = string
   default     = 443
 }
 
 variable "container_protocol" {
   description = "The protocol to use to connect with the container."
-  type        = "string"
+  type        = string
   default     = "HTTPS"
 }
 
 variable "allow_public_http" {
   description = "Allow inbound access from the Internet to port 80"
-  type        = "string"
+  type        = string
   default     = true
 }
 
 variable "allow_public_https" {
   description = "Allow inbound access from the Internet to port 443"
-  type        = "string"
+  type        = string
   default     = true
 }
+
